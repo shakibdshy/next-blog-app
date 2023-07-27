d375458ec813189d784fa5bd3dce5685eabbff42
shaibdshy <shaibdshy@gmail.com>
2023-07-27 21:42:29 +0600

    Fixed Sidebar Header

--- components/dashboard/primary-sidebar.tsx
+++ components/dashboard/primary-sidebar.tsx
@@ -1,11 +1,15 @@
+'use client'
+
 import { siteConfig } from '@/config/site'
 import Link from 'next/link'
 import React from 'react'
-import { buttonVariants } from '../ui/button'
 import { Icons } from '../icons'
 import { cn } from '@/lib/utils'
+import { usePathname } from 'next/navigation'
 
 function PrimarySidebar() {
+  const path = usePathname()
+
   return (
     <aside className='border-border sticky left-0 top-0 w-56 border-r border-solid px-3 py-6'>
       <nav className='flex flex-col gap-4'>
@@ -14,7 +18,7 @@ function PrimarySidebar() {
             // @ts-ignore
             const Icon = Icons[item.icon || "arrowRight"]
             return (
-              <Link href={item.href} className="hover:bg-primary hover:text-muted flex h-10 items-center rounded-md px-4 py-2 transition-all">
+              <Link href={item.href} className={cn("hover:bg-primary hover:text-primary-foreground flex h-10 items-center rounded-md px-4 py-2 text-sm font-semibold tracking-wider transition-all", path === item.href ? "bg-primary text-primary-foreground" : "")}>
                 <Icon className="mr-2 h-4 w-4" />
                 {item.title}
               </Link>
