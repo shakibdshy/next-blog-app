802d8c2284d23e0713435cd5d51fc859b1c3fba9
shaibdshy <shaibdshy@gmail.com>
2023-07-30 16:56:06 +0600

    Fixed Hydration Error

--- components/layout/main-nav.tsx
+++ components/layout/main-nav.tsx
@@ -5,51 +5,35 @@ import { NavItem } from "@/types/nav"
 import { siteConfig } from "@/config/site"
 import { cn } from "@/lib/utils"
 import { Icons } from "@/components/icons"
-import { Menu } from "lucide-react"
-import { useBreakpoint } from "@/hooks/usebreakpoints"
-import MobileMenuDropdown from "@/components/dialog/mobile-offcanves"
-import OffCanvasMenu from "@/components/dialog/offcanves-menu"
 
 interface MainNavProps {
   items?: NavItem[]
 }
 
 export function MainNav({ items }: MainNavProps) {
-  const breakpoint = useBreakpoint();
-  const isMobile = ['lg', 'xl', '2xl', '3xl', '4xl'].includes(breakpoint);
   return (
     <div className="flex w-full justify-between gap-6 md:gap-10 lg:w-auto lg:justify-start">
       <Link href="/" className="flex items-center space-x-2">
         <Icons.logo className="h-6 w-6" />
         <span className="inline-block font-bold">{siteConfig.name}</span>
       </Link>
-      {
-        isMobile && items?.length ? (
-          <div className="flex gap-6">
-            {items?.map(
-              (item, index) =>
-                item.href && (
-                  <Link
-                    key={index}
-                    href={item.href}
-                    className={cn(
-                      "text-muted-foreground hover:text-foreground flex items-center text-sm font-medium",
-                      item.disabled && "cursor-not-allowed opacity-80"
-                    )}
-                  >
-                    {item.title}
-                  </Link>
-                )
-            )}
-          </div>
-        )
-          :
-          <OffCanvasMenu
-            className="flex min-h-screen flex-col justify-between"
-            icon={<Menu />}
-            render={<MobileMenuDropdown />}
-          />
-      }
+      <div className="hidden gap-6 lg:flex">
+        {items?.map(
+          (item, index) =>
+            item.href && (
+              <Link
+                key={index}
+                href={item.href}
+                className={cn(
+                  "text-muted-foreground hover:text-foreground flex items-center text-sm font-medium",
+                  item.disabled && "cursor-not-allowed opacity-80"
+                )}
+              >
+                {item.title}
+              </Link>
+            )
+        )}
+      </div>
     </div>
   )
 }
--- components/layout/site-header.tsx
+++ components/layout/site-header.tsx
@@ -11,75 +11,79 @@ import { Modal } from "@/components/ui/modal"
 import { CustomTabs } from "@/components/ui/tab"
 import { SignedIn, SignedOut } from '@clerk/nextjs/app-beta/client'
 import { useUser } from "@clerk/nextjs"
-import { useBreakpoint } from "@/hooks/usebreakpoints"
 import { tabsContent, tabsList } from "@/config/tab-content"
 import { Dropdown } from "@/components/dropdown/dropdown"
 import { ProfileDropdown } from "@/components/dropdown/profile-dropdown"
+import { Menu } from "lucide-react"
+import MobileMenuDropdown from "@/components/dialog/mobile-offcanves"
+import OffCanvasMenu from "@/components/dialog/offcanves-menu"
 
 export function SiteHeader() {
   const { user } = useUser();
-  const breakpoint = useBreakpoint();
-  const isMobile = ['lg', 'xl', '2xl', '3xl', '4xl'].includes(breakpoint);
 
   return (
     <header className="bg-background sticky top-0 z-40 w-full border-b">
       <div className="container flex h-16 items-center space-x-4 px-4 sm:justify-between sm:space-x-0">
         <MainNav items={siteConfig.mainNav} />
-        {
-          isMobile && (
-            <div className="flex flex-1 items-center justify-end space-x-4">
-              <div className="flex items-center space-x-1">
-                <Link
-                  href={siteConfig.links.github}
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <span
-                    className={buttonVariants({
-                      size: "icon",
-                      variant: "ghost",
-                    })}
-                  >
-                    <Icons.gitHub className="h-5 w-5" />
-                    <span className="sr-only">GitHub</span>
-                  </span>
-                </Link>
-                <ThemeToggle />
-              </div>
-              <div className="flex items-center gap-4">
-                <SignedIn>
-                  {/* <UserButton afterSignOutUrl="/" /> */}
-                  <Dropdown
-                    variant="image"
-                    title="Profile"
-                    image={user?.imageUrl}
-                    render={
-                      <ProfileDropdown />
-                    }
-                  />
-                </SignedIn>
-                <SignedOut>
-                  {/* <SignInButton mode='modal'>
+        <div className="hidden flex-1 items-center justify-end space-x-4 lg:flex">
+          <div className="flex items-center space-x-1">
+            <Link
+              href={siteConfig.links.github}
+              target="_blank"
+              rel="noreferrer"
+            >
+              <span
+                className={buttonVariants({
+                  size: "icon",
+                  variant: "ghost",
+                })}
+              >
+                <Icons.gitHub className="h-5 w-5" />
+                <span className="sr-only">GitHub</span>
+              </span>
+            </Link>
+            <ThemeToggle />
+          </div>
+          <div className="flex items-center gap-4">
+            <SignedIn>
+              {/* <UserButton afterSignOutUrl="/" /> */}
+              <Dropdown
+                variant="image"
+                title="Profile"
+                image={user?.imageUrl}
+                render={
+                  <ProfileDropdown />
+                }
+              />
+            </SignedIn>
+            <SignedOut>
+              {/* <SignInButton mode='modal'>
                 <Button>
                   Sign in
                 </Button>
               </SignInButton> */}
-                  <Modal
-                    title="Sign in"
-                    titleClassName={buttonVariants({ variant: "default", className: 'hover:text-primary-foreground' })}
-                    render={
-                      <CustomTabs
-                        defaultValue="signIn"
-                        tabsList={tabsList}
-                        tabsContent={tabsContent}
-                      />
-                    }
+              <Modal
+                title="Sign in"
+                titleClassName={buttonVariants({ variant: "default", className: 'hover:text-primary-foreground' })}
+                render={
+                  <CustomTabs
+                    defaultValue="signIn"
+                    tabsList={tabsList}
+                    tabsContent={tabsContent}
                   />
-                </SignedOut>
-              </div>
-            </div>
-          )
-        }        
+                }
+              />
+            </SignedOut>
+          </div>
+        </div>
+
+        <div className="block lg:hidden">
+          <OffCanvasMenu
+            className="flex min-h-screen flex-col justify-between"
+            icon={<Menu />}
+            render={<MobileMenuDropdown />}
+          />
+        </div>
       </div>
     </header>
   )
